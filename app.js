c8e725ff623807418091569799e5306513826c5b
Alex Hunt <hello@alexhunt.io>
2017-02-05 23:12:51 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const supervisor = require('supervisord-eventlistener');
+const fetch = require('node-fetch');
+
+require('dotenv').config();
+
+const messages = require('./messages.json');
+
+supervisor.on('event', (type, headers, data) => {
+    if (data.groupname === process.env.PROCESS_GROUP_NAME && Object.keys(messages).includes(type)) {
+        fetch(process.env.WEBHOOK_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                content: `[${process.env.SERVER_NAME}] ${messages[type]}`
+            })
+        })
+        .catch(console.error);
+    }
+});
+
+supervisor.listen(process.stdin, process.stdout);
