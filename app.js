fea70744a07bac690cf76ea74d42aa610daabfa0
Alex Hunt <hello@alexhunt.io>
2017-06-10 22:42:47 +0100

    Move request logic to WebhookPublisher class

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 const supervisor = require('supervisord-eventlistener');
 const fetch = require('node-fetch');
+const WebhookPublisher = require('./utils/webhook-publisher');
 
 require('dotenv').config();
 
 const messages = require('./config/messages.json');
 
 supervisor.on('event', (type, headers, data) => {
+    const publisher = new WebhookPublisher(process.env.WEBHOOK_URL, process.env.SERVER_NAME);
+
     if (data.groupname === process.env.PROCESS_GROUP_NAME && Object.keys(messages).includes(type)) {
-        fetch(process.env.WEBHOOK_URL, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                content: `[${process.env.SERVER_NAME}] ${messages[type]}`
-            })
-        })
-        .catch(console.error);
+        publisher.postStatus(messages[type])
+            .catch(console.error);
     }
 });
 
