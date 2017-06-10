b3e1fef823fd4374c9779a68f0bfad6048382d40
Alex Hunt <hello@alexhunt.io>
2017-06-10 22:44:40 +0100

    Implement maintenance mode message handling

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
+const fs = require('fs');
 const supervisor = require('supervisord-eventlistener');
-const fetch = require('node-fetch');
 const WebhookPublisher = require('./utils/webhook-publisher');
 
 require('dotenv').config();
@@ -9,7 +9,16 @@ const messages = require('./config/messages.json');
 supervisor.on('event', (type, headers, data) => {
     const publisher = new WebhookPublisher(process.env.WEBHOOK_URL, process.env.SERVER_NAME);
 
-    if (data.groupname === process.env.PROCESS_GROUP_NAME && Object.keys(messages).includes(type)) {
+    if (fs.existsSync('MAINTENANCE_MODE')) {
+        let downState = JSON.parse(fs.readFileSync('MAINTENANCE_MODE', 'utf-8'));
+
+        if (!('notified' in downState)) {
+            downState.notified = true;
+            publisher.postStatus('Server is down for maintenance')
+                .catch(console.error)
+                .then(fs.writeFileSync('MAINTENANCE_MODE', JSON.stringify(downState) + '\n', 'utf-8'));
+        }
+    } else if (data.groupname === process.env.PROCESS_GROUP_NAME && Object.keys(messages).includes(type)) {
         publisher.postStatus(messages[type])
             .catch(console.error);
     }
